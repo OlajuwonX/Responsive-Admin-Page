455fb39da879628a7a858727e72e6e23b72539b2
OlajuwonX <olasimboolajuwon@gmail.com>
2024-07-31 16:25:22 +0100

    save all files

---
+++ orders.js
@@ -0,0 +1,21 @@
+
+const Orders = [
+    {
+        productName: 'JavaScript Tutorial',
+        productNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'Css Full Course',
+        productNumber: '97245',
+        PaymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Flex-Box Tutorial',
+        productNumber: '36452',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
